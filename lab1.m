c26fad9741195ef1fc46c0ce1cee48e28caf29a8
mkownak <92052799+mkownak@users.noreply.github.com>
2023-10-14 16:31:31 +0200

    Update lab1.m

--- lab1.m
+++ lab1.m
@@ -27,6 +27,11 @@ ahh = ah' * [1;1000]
 
 % zad.7
 % skip
+m1=[3,1;-1 1]^-1
+m2=[31;-9]
+odp = m1 * m2
+x1=odp(1)
+x2=odp(end)
 
 % zad.8
 a = [1:3;4:6;7:9]
@@ -38,4 +43,28 @@ c=b;c(b > 2 & b < 5) = c(b > 2 & b < 5)+10
 
 % zad.10
 d=[1:3;4,5,4;7,8,5]
+d-mean(d)
+
+% zad.11
+e = [1:3;4,5,4]
+wymiary=size(e)
+e2 = -1 + 2 * rand(wymiary(1), wymiary(2))
+
+% zad.12
+f = floor(5*rand(4,3))
+for i = 1:size(f,1)
+  for j = 2:size(f,2)
+    f(i,j) = f(i,j) + f(i,j-1);
+  end
+end
+
+% zad.12
+g = [2,3,0,0;4,3,4,1;4,2,3,1]
+for i = 1:size(g,1)
+  for j = 1:size(g,2)
+    if g(i,j) > 3
+      g(i,j) = 0;
+    endif
+  endfor
+ endfor5]
 d-mean(d)
