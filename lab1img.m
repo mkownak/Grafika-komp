f02e54cf1c711db808279eafd1de72244f55c49f
mkownak <92052799+mkownak@users.noreply.github.com>
2023-10-14 18:41:54 +0200

    Update lab1img.m

--- lab1img.m
+++ lab1img.m
@@ -1,1 +1,18 @@
 
+
+% zad.1
+img_uint8=imread('rzeczka.jpg');
+
+img_uint8(:,10:50,1)=255;
+img_uint8(:,10:50,2)=255;
+img_uint8(:,10:50,3)=0;
+
+imshow(img_uint8);
+
+% zad.3
+clear
+img_uint8=imread('rzeczka.jpg')
+negatyw=255-img_uint8;
+imshow(negatyw);
+
+% zad.2
