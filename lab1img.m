3094410d99cd97ea70fb3d14df84411903c097a6
mkownak <92052799+mkownak@users.noreply.github.com>
2023-10-16 12:58:32 +0200

    Update lab1img.m

--- lab1img.m
+++ lab1img.m
@@ -16,3 +16,23 @@ negatyw=255-img_uint8;
 imshow(negatyw);
 
 % zad.2
+% Load your image
+originalImage = imread('rzeczka.jpg');
+
+% Get the dimensions of the original image
+[rows, cols, channels] = size(originalImage);
+
+% Create an empty matrix to store the mirrored image
+mirroredImage = zeros(rows, cols, channels, class(originalImage));
+
+% Mirror the image by copying pixels in reverse order along the horizontal axis
+for i = 1:rows
+    for j = 1:cols
+        mirroredImage(i, j, :) = originalImage(i, cols - j + 1, :);
+    end
+end
+
+% Display the mirrored image
+#imshow(mirroredImage);
+new_image=[originalImage,mirroredImage]
+imshow(new_image)
